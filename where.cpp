d3fc31fc47cceb97b90ad06b0de189849b73caad
weiqixu05 <weiqixu2005@icloud.com>
2024-03-05 12:39:51 -0500

    changes

--- where.cpp
+++ where.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
+vector<string> list_of_restaurants;
+
 int main() {
-    
+    string s;
+    while (cin >> s) {
+        cout << "add restaurant:\n";
+    }
+    for(int i = 0; i < list_of_restaurants.size(); i++) {
+        printf("%s", list_of_restaurants[i].c_str());
+    }
 }
